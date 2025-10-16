d3251036ae9fca228114096522554ad60dea79e7
nicunursekatie <katielong2316@gmail.com>
2025-10-16 01:46:31 -0400

    Load responsive styles via index stylesheet

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './styles/mobile-responsive.css';
 import { ThemeProvider } from '@/context/theme-provider';
 
 createRoot(document.getElementById('root')!).render(
