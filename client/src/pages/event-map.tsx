7fca625018031657ca2430817009289f3bae4fe5
Katie Long <katielong2316@gmail.com>
2025-11-18 11:39:55 -0500

    Update event map tile layer URL to use new raster tiles and mark subproject commit as dirty to indicate ongoing changes.

--- client/src/pages/event-map.tsx
+++ client/src/pages/event-map.tsx
@@ -683,7 +683,7 @@ export default function EventMapView() {
             >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
                 subdomains="abcd"
                 maxZoom={20}
               />
