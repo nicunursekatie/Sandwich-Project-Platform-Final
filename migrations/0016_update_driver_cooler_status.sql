58aed45f6c4b1fe23b14694bd6961886a3704635
nicunursekatie <katielong2316@gmail.com>
2026-03-03 21:05:58 +0000

    delete old migrations

--- migrations/0016_update_driver_cooler_status.sql
+++
@@ -1,2 +0,0 @@
--- Add cooler_status column if it doesn't exist
-ALTER TABLE "drivers" ADD COLUMN IF NOT EXISTS "cooler_status" text;
--- migrations/0043_add_official_sheet_timestamp.sql
+++
@@ -1,6 +0,0 @@
--- Migration: Add addedToOfficialSheetAt timestamp to event_requests
--- Date: 2026-02-22
--- Description: Track when an event was added to the official Google Sheet
-
-ALTER TABLE event_requests
-ADD COLUMN IF NOT EXISTS added_to_official_sheet_at TIMESTAMP;
