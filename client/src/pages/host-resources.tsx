2c6b08d2249aed9f1249188a680fd9dea9bd80f9
nicunursekatie <katie@thesandwichproject.org>
2026-03-03 22:43:42 -0500

    Update recipient database link in HostResources component

--- client/src/pages/host-resources.tsx
+++ client/src/pages/host-resources.tsx
@@ -444,7 +444,7 @@ export default function HostResources() {
             title="Recipient Database"
             description="Browse the database of recipient organizations that receive sandwiches."
             icon={Users}
-            href="dashboard?section=recipients"
+            href="recipients"
           />
         </div>
       </section>
