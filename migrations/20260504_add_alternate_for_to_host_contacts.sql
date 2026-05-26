dc99105419bcbbe3c8369c147674370371f805d5
nicunursekatie <katielong2316@gmail.com>
2026-05-26 21:51:28 +0000

    migration cleanup

--- migrations/20260504_add_alternate_for_to_host_contacts.sql
+++
@@ -1,3 +0,0 @@
--- Add alternate_for_contact_id to host_contacts table
--- Tracks which host contact this alternate is standing in for
-ALTER TABLE host_contacts ADD COLUMN IF NOT EXISTS alternate_for_contact_id INTEGER;
--- migrations/20260507_add_ein_to_recipients.sql
+++
@@ -1,2 +0,0 @@
--- Add EIN (Employer Identification Number) field to recipients table
-ALTER TABLE recipients ADD COLUMN IF NOT EXISTS ein TEXT;
